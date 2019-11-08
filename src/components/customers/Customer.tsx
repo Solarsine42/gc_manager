88c0e336931712374c458793592d6cd2a1f70f70
Solarsine42 <Solarsine42@gmail.com>
2019-11-07 20:25:24 -0700

    teetimes added to customer ui

--- src/components/customers/Customer.tsx
+++ src/components/customers/Customer.tsx
@@ -10,25 +10,36 @@ interface CPropsTypes {
 }
 
 export const Customer: React.FC<CPropsTypes> = props => {
-  return (
-    <div className="container row mb-3">
-      <h5 className="col">{props.customer.name}</h5>
-      <Dropdown className="col">
-        <Dropdown.Toggle size="sm" variant="success" id="dropdown-button">
-          Options
-        </Dropdown.Toggle>
+  const teeTimeList = props.customer.teetimes.map((ttime: any) => (
+    <p>{ttime.time}</p>
+  ));
 
-        <Dropdown.Menu>
-          <Dropdown.Item>Add Tee Time</Dropdown.Item>
-          <Dropdown.Item>Remove Tee Time</Dropdown.Item>
-          <Dropdown.Item style={{ color: "orange" }}>
-            Edit Customer
-          </Dropdown.Item>
-          <Dropdown.Item style={{ color: "red" }}>
-            Delete Customer
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+  return (
+    <div>
+      <div className="container row mb-3">
+        <h5 className="col">{props.customer.name}</h5>
+        <Dropdown className="col">
+          <Dropdown.Toggle size="sm" variant="success" id="dropdown-button">
+            Options
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item>Add Tee Time</Dropdown.Item>
+            <Dropdown.Item>Remove Tee Time</Dropdown.Item>
+            <Dropdown.Item style={{ color: "orange" }}>
+              Edit Customer
+            </Dropdown.Item>
+            <Dropdown.Item style={{ color: "red" }}>
+              Delete Customer
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+      <div>
+        <p>
+          {props.customer.teetimes[0] ? teeTimeList : "No Tee Times listed"}
+        </p>
+      </div>
     </div>
   );
 };
