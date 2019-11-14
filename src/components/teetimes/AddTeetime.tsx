fb1e27858d036ba87bc7478b8d916fd7bdef7a0e
Solarsine42 <Solarsine42@gmail.com>
2019-11-13 21:35:44 -0700

    add tee time datepicker

--- src/components/teetimes/AddTeetime.tsx
+++ src/components/teetimes/AddTeetime.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { Form, Button } from "react-bootstrap";
 import { addTeetime } from "../../store/teetimes/actions";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 interface ATTPropsTypes {
   location: any;
@@ -11,12 +13,16 @@ interface ATTPropsTypes {
 }
 
 interface stateTypes {
-  time: string;
+  time: any;
 }
 
 class AddTeetime extends React.Component<ATTPropsTypes, stateTypes> {
   state = {
     time: ""
+  };
+
+  handleDate = (date: any) => {
+    this.setState({ time: date });
   };
 
   handleSubmit = (e: any) => {
@@ -34,23 +40,35 @@ class AddTeetime extends React.Component<ATTPropsTypes, stateTypes> {
 
   render() {
     return (
-      <div style={{ width: "70%" }} className="container">
+      <div
+        style={{ marginLeft: "25%", marginRight: "25%", width: "50%" }}
+        className="container"
+      >
         <h3>Add New Tee Time</h3>
         <br />
         <Form onSubmit={this.handleSubmit}>
           <Form.Group className="row">
-            <Form.Label className="col-3">Time:</Form.Label>
-            <Form.Control
+            <Form.Label>Time:</Form.Label>
+            <DatePicker
+              onChange={(date: any) => this.handleDate(date)}
+              placeholderText={this.state.time}
+              showTimeSelect
+              timeFormat="hh:mm aa"
+              timeIntervals={15}
+              timeCaption="Time"
+              dateFormat="MMMM d, yyyy hh:mm aa"
+            />
+            {/* <Form.Control
               className="col-7"
               value={this.state.time}
               onChange={(e: any) => this.setState({ time: e.target.value })}
               name="time"
               type="text"
               required
-            />
+            /> */}
           </Form.Group>
           <Button
-            style={{ marginLeft: "35%", marginRight: "35%", width: "30%" }}
+            // style={{ marginLeft: "25%", marginRight: "25%", width: "30%" }}
             variant="primary"
             type="submit"
           >
